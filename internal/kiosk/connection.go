5fea6e731baaad9a9c435fc75487dea0f57d244c
aasset <Gontabek@gmail.com>
2024-12-05 15:50:58 +0500

    connection server url ping added

--- internal/kiosk/connection.go
+++ internal/kiosk/connection.go
@@ -10,21 +10,43 @@ import (
 
 var lastConnectionState = true
 
-func connected() bool {
+func connected(cfg *config.Config) (bool, error) {
 	client := &http.Client{
 		Timeout: 5 * time.Second,
 	}
 	resp, err := client.Get("http://clients3.google.com/generate_204")
 	if err != nil {
-		return false
+		return false, err
 	}
+
 	resp.Body.Close()
-	return true
+
+	respServer, err := client.Get(cfg.ServerURL)
+	if err != nil {
+		return false, err
+	}
+
+	respServer.Body.Close()
+
+	if !(respServer.StatusCode >= 200 && respServer.StatusCode < 300) {
+		logger.Warn("Ping failed: %s (Status: %s)\n", cfg.ServerURL, resp.Status)
+	}
+
+	return true, nil
 }
 
 func ReloadChromiumPage(cfg *config.Config) {
 	logger.Info("Reloading Chromium page...")
-	_, err := exec.Command("xdotool","search", "--onlyvisible", "--class", "chromium", "windowfocus", "key", "F5").Output()
+	_, err := exec.Command(
+		"xdotool",
+		"search",
+		"--onlyvisible",
+		"--class",
+		"chromium",
+		"windowfocus",
+		"key",
+		"F5",
+	).Output()
 	if err != nil {
 		logger.Error("Failed to reload Chromium page:", err)
 	}
@@ -33,7 +55,10 @@ func ReloadChromiumPage(cfg *config.Config) {
 func MonitorConnection(cfg *config.Config) {
 	for {
 		time.Sleep(5 * time.Second)
-		currentState := connected()
+		currentState, err := connected(cfg)
+		if err != nil {
+			logger.Warn("Error accured while trying to check connection:", err)
+		}
 
 		if currentState && !lastConnectionState {
 			logger.Info("Internet connection restored")
